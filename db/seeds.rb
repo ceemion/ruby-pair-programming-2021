facfb6e5286594648556d2819224aa7944c5ee9b
ceemion <fiyinsimeon@gmail.com>
2021-07-26 16:38:11 +0100

    Add seed data.

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+users = User.create([{name: "Fiyin"}, {name: "Jane Doe"}])
+
+question_one = Question.create(title: "What is your name?", is_private: true, user: users.first)
+Answer.create(body: "My name is Fiyin", question_id: question_one.id, user: users.first)
+
+question_two = Question.create(title: "What languages do you speak?", is_private: false, user: users.second)
+Answer.create(body: "I speak English and French", question_id: question_two.id, user: users.second)
