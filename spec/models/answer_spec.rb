da06ef432c6fe79938d25b1bbc1db3d283c029fa
ceemion <fiyinsimeon@gmail.com>
2021-07-26 15:50:26 +0100

    Add basic tests for Question, Answer and User models.

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Answer, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "answer" do
+    it "creates is a valid answer" do
+      user = User.create(name: "Fiyin")
+      question = Question.create(title: "A question", is_private: true, user_id: user.id)
+      answer = Answer.create(body: "Answer to your question", question_id: question.id, user_id: user.id)
+
+      expect(answer).to be_valid
+    end
+  end
 end
--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Question, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "question" do
+    it "creates is a valid question" do
+      user = User.create(name: "Fiyin")
+      question = Question.create(title: "A question", is_private: true, user_id: user.id)
+      
+      expect(question).to be_valid
+    end
+  end
 end
