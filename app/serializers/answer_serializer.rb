fa935b293e7c58173719b625e2b8bc10c8813960
ceemion <fiyinsimeon@gmail.com>
2021-08-04 20:49:22 +0100

    Return the user for each answer.

--- app/serializers/answer_serializer.rb
+++ app/serializers/answer_serializer.rb
@@ -1,4 +1,8 @@
 class AnswerSerializer < ActiveModel::Serializer
-  attributes :id, :body
+  attributes :id, :body, :user
   belongs_to :user
+
+  def user
+    User.select(:id, :name).find_by(id: object.user_id)
+  end
 end
