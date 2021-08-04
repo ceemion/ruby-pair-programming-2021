1165a7388e8474b15b6ec9757a6878b5ba463e19
ceemion <fiyinsimeon@gmail.com>
2021-08-04 22:42:48 +0100

    Require every API request to include a valid Tenant API key

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,18 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::API
+  before_action :authenticate
+
+  include ActionController::HttpAuthentication::Basic::ControllerMethods
+  include ActionController::HttpAuthentication::Token::ControllerMethods
+
+  private
+
+  # This will raise an error and automatically respond with a 401 HTTP status
+  # code when API key authentication fails
+  def authenticate
+    authenticate_or_request_with_http_token do |http_token, options|
+      @current_tenant = Tenant.find_by(api_key: http_token)
+    end
+  end
 end
